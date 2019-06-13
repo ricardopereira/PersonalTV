d2bed574e55565d0f92b3cf1ac10c752912697a4
Ricardo Pereira <m@ricardopereira.eu>
2019-06-13 11:10:03 +0100

    Do not check versions

--- PopcornTime/Startup/AppDelegate.swift
+++ PopcornTime/Startup/AppDelegate.swift
@@ -165,7 +165,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegat
     
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        UpdateManager.shared.checkVersion(.daily)
+        //UpdateManager.shared.checkVersion(.daily)
     }
     
     func awakeObjects() {
